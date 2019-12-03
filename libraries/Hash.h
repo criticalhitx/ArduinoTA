29de79b0d916a150d96ac455b619155d192079b4
criticalhitx <clink200032@gmail.com>
2019-12-04 04:34:49 +0700

    4 Dec 2019 , added SHA and no transmission of stealth address.

---
+++ libraries/Hash.h
@@ -0,0 +1,21 @@
+[-u,-tsu,ru(selain iru dan eru) --> tte]
+au -> atte -> atta
+kau -> katte -> katta
+matsu -> matte ->matta
+naru -> natte -> natta
+
+[gu]
+oyogu -> oyoide -> oyoida
+[mu,bu]
+nomu  -> nonde -> nonda
+asobu -> asonde -> asonda
+
+[ku]
+iku -> itte (exception)
+kaku-> kaite -> kaita
+kiku -> kiite -> kiita
+
+
+[-iru,eru -> -ta)
+miru -> mite -> mita
+taberu-> tabete -> tabete
